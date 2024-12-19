87984432492f47da716fc6499a8e47652d5a47d5
Murugan-S1234 <murugansiva2193@gmail.com>
2024-12-19 13:59:30 +0530

    add env file from frontend folder and update SendOtp.js and ValidateOtp.js in components folder

--- backend/server.js
+++ backend/server.js
@@ -93,5 +93,5 @@ app.post("/validate-otp", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
